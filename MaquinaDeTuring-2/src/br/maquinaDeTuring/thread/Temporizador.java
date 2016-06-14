45b28a1888886967210bd12648f9166e59c47eb7
tiagoaleff <tiago.aleff@gmail.com>
2016-06-14 03:06:04 -0300

    alteraçoes em funcionalidades. 	- analise da fita melhorada; 	- implementando a thread

---
+++ MaquinaDeTuring-2/src/br/maquinaDeTuring/thread/Temporizador.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.maquinaDeTuring.thread;
+
+/**
+ *
+ * @author tiagoaleff
+ */
+public class Temporizador implements Runnable{
+
+    
+    private MaquinaModel
+    
+    
+    @Override
+    public void run() {
+
+    }
+    
+}
